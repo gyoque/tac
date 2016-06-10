e2c566852e35ffd46fa38773c074c7b5e46538d6
gyoque <g.yoque@gmail.com>
2016-06-10 08:39:04 -0700

    David Remove Try Catch
    troubleshooting

--- PROJECT FILES/ProjectApp/input_contactInfo.php
+++ PROJECT FILES/ProjectApp/input_contactInfo.php
@@ -3,35 +3,33 @@ $servername = "localhost";
 $username = "root";
 $password = "artsApply2!";
 $dbname = "tac";
-try{
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+//try{
+
+  $conn = new PDO("mysql:dbname=$dbname;host={$servername}", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
+  echo "Connected successfully";
 
   // //CREATE PROJECT with ID and Title but no other values
-  // $createProj = "INSERT INTO `ProjectInfo`(`title`)
-  //     VALUES ('$_POST[projTitle]')";
+  $createProj = "INSERT INTO `ProjectInfo`(`title`)
+       VALUES ('$_POST[projTitle]')";
   //
-  // $conn->exec($createProj);
+  $conn->exec($createProj);
   //
   // //get projectID of newly created Project
-  // $last_id = $conn->lastInsertId();
+  $last_id = $conn->lastInsertId();
 
   //echo "Project Title:  ".$_POST[projTitle]."  Project ID is: ".$last_id;
 
   //echo "".$last_id.", ".$_POST[first].", ".$_POST[last].", ".$_POST[cStreet].", ".$_POST[cCity].", ".$_POST[cState].", ".$_POST[cZip].", ".$_POST[cEmail].", ".$_POST[cPhone];
 
   //insert new contact information
-  // $addContact = "INSERT INTO `ContactInfo`(`projectID`, `firstName`, `lastName`, `street`, `city`, `state`, `zip`, `email`, `dayPhone`)
-  //         VALUES ('$last_id', '$_POST[first]', '$_POST[last]', '$_POST[cStreet]',
-  //         '$_POST[cCity]', '$_POST[cState]', '$_POST[cZip]',
-  //         '$_POST[cEmail]', '$_POST[cPhone]')";
+  $addContact = "INSERT INTO `ContactInfo`(`projectID`, `firstName`, `lastName`, `street`, `city`, `state`, `zip`, `email`, `dayPhone`) VALUES ('$last_id', '$_POST[first]', '$_POST[last]', '$_POST[cStreet]', '$_POST[cCity]', '$_POST[cState]', '$_POST[cZip]', '$_POST[cEmail]', '$_POST[cPhone]')";
 
-  $addContact = "INSERT INTO `ContactInfo` (`projectID`, `firstName`, `lastName`, `street`, `city`, `state`, `zip`, `email`, `dayPhone`)
-        VALUES ('19', 'Carly', 'Brock', '1500 N Warner', 'Tacoma', 'WA', '98416', 'carly@email.com', '2538791414')";
+  $addContact2 = "INSERT INTO `ContactInfo` (`projectID`, `firstName`, `lastName`, `street`, `city`, `state`, `zip`, `email`, `dayPhone`) VALUES ('19', 'Carly', 'Brock', '1500 N Warner', 'Tacoma', 'WA', '98416', 'carly@email.com', '2538791414')";
 
-  //echo "".$addContact;
+  echo "<br>$addContact<br/>$addContact2";
   $conn->exec($addContact);
 
   // $sql = $createProj.";".$addContact.";";
@@ -43,7 +41,6 @@ try{
   // //encrypt password
   // $enc = openssl_encrypt('$_POST[password]','aes128','aRTSaPPcrYPT');
   //
-  //ADAPT THIS SECTION****
   // if($enc == FALSE){
   //   $passErr = 'Password could not be stored';
   //   $success = false;
@@ -60,9 +57,11 @@ try{
 
 
   //header("Location: /generalInfo.php");
-}
-catch(PDOException $e){
-  echo "Connection Failed: ".$e->getMessage();
-}
+//}
+
+//catch(PDOException $e){
+ // echo "Connection Failed: ".$e->getMessage();
+//  die();
+//}
 
 ?>
